da16391e855b639f4d38d8ccd3962b8ac2bc6efa
AgataBialoskorska <agatamirk@gmail.com>
2023-10-19 15:07:28 +0200

    add skills

--- src/components/SkillsTemplate.vue
+++ src/components/SkillsTemplate.vue
@@ -14,23 +14,46 @@
 
 <style scoped>
 .window {
+	display: flex;
+	flex-direction: column;
+    justify-content: space-around;
+	align-items: center;
+	position: relative;
 	border: 1px outset;
 	border-radius: 15px;
-	flex-direction: column;
-	place-items: center;
-
-	margin: auto;
-	margin-top: 5%;
-	min-height: 40vh;
-	width: 60%;
-	display: grid;
-	grid-template-rows: 0.3fr 0.3fr 0.4fr;
-	grid-template-columns: 1fr;
+	margin-top: 2em;
 }
 div.window > * {
 	display: flex;
+	padding: 10px;
+	text-align: center;
 }
 
 @media (min-width: 1024px) {
+}
+.window:before {
+	content: ' ';
+	border-left: 1px solid var(--color-border);
+	position: absolute;
+	left: 50%;
+	bottom: 0;
+	height: calc(1em - 10px);
+}
+
+.window:after {
+	content: ' ';
+	border-left: 1px solid var(--color-border);
+	position: absolute;
+	left: 50%;
+	top: 0;
+	height: 0;
+}
+
+.window:first-of-type:before {
+	display: none;
+}
+
+.window:last-of-type:after {
+	display: none;
 }
 </style>
