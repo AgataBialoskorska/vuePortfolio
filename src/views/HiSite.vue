7706de1f4235b7da1db99f5e9eec3b7c6adb3052
AgataBialoskorska <agatamirk@gmail.com>
2023-11-14 09:52:46 +0100

    setup the main view

---
+++ src/views/HiSite.vue
@@ -0,0 +1,15 @@
+<template name="hi">
+		<img
+			alt="my photo"
+			class="photo"
+			src="@/components/img/agataCV.jpeg" />
+		<h1 class="green">Hi, I am Agata</h1>
+</template>
+
+<style scoped>
+.photo {
+	border: none;
+	border-radius: 50%;
+	width: 40%;
+}
+</style>
---
+++ src/views/Navigation.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template name="nav">
+	<nav>
+		<RouterLink to="/">Home</RouterLink>
+		<RouterLink to="/stack">Tech Stack</RouterLink>
+		<RouterLink to="/projects">Projects</RouterLink>
+	</nav>
+</template>
+<style scoped>
+nav {
+	width: 100%;
+	font-size: 1rem;
+	text-align: center;
+	padding: 2rem 0;
+}
+
+nav a.router-link-exact-active {
+	color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+	background-color: transparent;
+}
+
+nav a {
+	display: inline-block;
+	padding: 0 1rem;
+	border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+	border: 0;
+}
+
+@media (min-width: 1024px) {
+	nav {
+		margin-left: -1rem;
+		font-size: 1.5rem;
+	}
+}
+</style>
