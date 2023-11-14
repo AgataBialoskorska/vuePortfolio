1a0eba61fe3a644f0048c657c4aca202b5aaba38
AgataBialoskorska <agatamirk@gmail.com>
2023-11-14 13:41:22 +0100

    refactor project page

--- src/components/TemplateProjects.vue
+++ src/components/TemplateProjects.vue
@@ -1,49 +1,52 @@
 <template>
 	<div class="window">
 		<h3>
-			<slot name="headings"></slot>
+			<slot name="title"></slot>
 		</h3>
 		<i>
-			<slot name="image"></slot>
+			<slot name="left"></slot>
 		</i>
 		<p>
-			<slot name="description"></slot>
+			<slot name="right"></slot>
 		</p>
 	</div>
 </template>
 
 <style scoped>
-.window {
-	margin: 0.5em auto;
-	display: grid;
-	grid-template-areas:
-		'header header'
-		'image description';
-	border: 2px outset;
-	border-radius: 15px;
-	transform: scale(1);
-	width:80%;
-}
-.window:hover {
-	background-color: black;
-	transform: scale(1.2);
-	z-index: 99;
-}
-.window > * {
-	display: flex;
-	padding: 10px;
-}
+	.window {
+		margin: 0.5em auto;
+		display: grid;
+		grid-template-areas:
+			'title right'
+			'left right';
+		grid-template-columns: 0.7fr 1fr;
+		border: 2px outset;
+		border-radius: 15px;
+		transform: scale(1);
+		transition: all 500ms;
+		width: 80%;
+	}
+	.window:hover {
+		background-color: black;
+		transform: scale(1.05);
+		transition: transform 500ms ease-in-out;
+		z-index: 99;
+	}
+	.window > * {
+		display: flex;
+		align-items: center;
+		padding: 10px;
+	}
 
-.window > h3 {
-	grid-area: header;
-}
+	.window > h3 {
+		grid-area: title;
+	}
 
-.window > i {
-	grid-area: image;
-	width: 100px;
-}
+	.window > i {
+		grid-area: left;
+	}
 
-.window > p {
-	grid-area: description;
-}
+	.window > p {
+		grid-area: right;
+	}
 </style>
