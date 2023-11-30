fcbbd0b2dcf9ac76d45fb3c3c5247cb0051bf362
AgataBialoskorska <agatamirk@gmail.com>
2023-11-30 10:47:13 +0100

    add background and z-index to cookie

--- src/components/Cookies.vue
+++ src/components/Cookies.vue
@@ -29,23 +29,25 @@
 
 <style>
 	.cookie {
+		background-color: var(--background);
 		display: flex;
 		margin: auto;
-		padding: 0 1vmin;
+		padding: 1vmin;
 		justify-content: space-evenly;
 		align-items: center;
 		font-size: 1rem;
+		z-index: 999;
 	}
 	button {
-        background: transparent;
-        border: 2px solid var(--green);
+		background: transparent;
+		border: 2px solid var(--green);
 		border-radius: 50%;
-        color: white;
+		color: white;
 		padding: 1em;
 	}
 	button:hover {
 		cursor: pointer;
-        background: var(--green);
-        border: 2px solid transparent;
+		background: var(--green);
+		border: 2px solid transparent;
 	}
 </style>
