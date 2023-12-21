9ad92a39013cac0892fad25a665154e00f40f991
AgataBialoskorska <agatamirk@gmail.com>
2023-12-21 12:30:29 +0100

    fix router tests - learning

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 
-// Import dynamic components
-const ViewHome = () => import('@/views/View-Home.vue');
-const ViewStack = () => import('@/views/View-Stack.vue');
-const ViewProjects = () => import('@/views/View-Projects.vue');
-const GameHangman = () => import('@/components/GameHangman.vue');
+const ViewHome = () => import('@/views/View-Home.vue')
+const ViewStack = () => import('@/views/View-Stack.vue')
+const ViewProjects = () => import('@/views/View-Projects.vue')
+const GameHangman = () => import('@/components/GameHangman.vue')
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: ViewHome,
-  },
-  {
-    path: '/stack',
-    name: 'Stack',
-    component: ViewStack,
-  },
-  {
-    path: '/projects',
-    name: 'Projects',
-    component: ViewProjects,
-  },
-  {
-    path: '/hangman',
-    name: 'Hangman',
-    component: GameHangman,
-  },
-  // ... other routes
-];
+	{
+		path: '/',
+		name: 'Home',
+		component: ViewHome,
+	},
+	{
+		path: '/stack',
+		name: 'Stack',
+		component: ViewStack,
+	},
+	{
+		path: '/projects',
+		name: 'Projects',
+		component: ViewProjects,
+	},
+	{
+		path: '/hangman',
+		name: 'Hangman',
+		component: GameHangman,
+	},
+]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
-
-export default router;
+	history: createWebHistory(),
+	routes: routes,
+})
+export { routes }
+export default router
